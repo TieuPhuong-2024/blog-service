ccbbea120484d3c7b00387d1a9626b709a689941
TieuPhuong-2024 <thamphuong.crochet@gmail.com>
2025-10-25 22:28:15 +0700

    refactor(controllers): update API endpoint path for categories
    Changed the request mapping in `CategoryController` from `/api/v1/post-categories` to `/api/v1/categories` to enhance clarity and better reflect the purpose of the endpoint.

--- src/main/java/org/crochet/blog/controller/CategoryController.java
+++ src/main/java/org/crochet/blog/controller/CategoryController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/post-categories")
+@RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
